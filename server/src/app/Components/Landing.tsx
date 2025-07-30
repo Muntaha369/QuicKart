89768f92c7ad70077e7d502655c76430e370c495
Muntaha <muntahasayed396@gmail.com>
2025-07-31 02:07:59 +0530

    Cleaned styling

--- server/src/app/Components/Landing.tsx
+++ server/src/app/Components/Landing.tsx
@@ -6,27 +6,25 @@ const inter = Inter({subsets:['latin']})
 
 const Landing = () => {
   return (
-    <div className='flex flex-row justify-between'>
-      <div className='z-10 w-screen sm:w-auto flex justify-center items-center flex-col '>
-      <h1 className={` ${inter.className} text-[5.2rem] sm:text-8xl text-white font-bold text-shadow-lg/15`}>Quic<span className='text-black'>kart</span></h1>
+    <div className='Hero'>
+      <div className='HeroText'>
+      <h1 className={` ${inter.className} QuickHero`}>Quic<span className='text-black'>kart</span></h1>
 
-      <div className={`${inter.className} sm:ml-15 flex font-semibold flex-col w-80 sm:w-96 mt-8 text-3xl sm:text-[2rem]`}>
+      <div className={`${inter.className} Herosubheading`}>
         <div className=' h-[35px] sm:h-[40px]'>
           <p>India's Fastest</p>
         </div>
-        <div className='flex justify-end items-center h-[35px] sm:h-[40px] text-[#5E2615]'>
+        <div className='subStyle'>
           <p>Delivering app</p>
         </div>
       </div>
 
       <button 
-      className={`${inter.className} text-white font-bold hover:scale-110 transition-all hover:cursor-pointer 
-      active:bg-[#e16843] active:scale-105 text-3xl sm:text-4xl px-7 mt-8 pb-2 pt-2 flex justify-center border border-[#5E2615] items-center 
-      rounded-xl bg-[#FF6C41]`}>
+      className={`${inter.className} HeroButton`}>
       Shop Now</button>
       </div>
 
-      <img className=' hidden sm:flex sm:right-[-300px] md:right-[-220px] h-[37rem] z-0 absolute top-0 lg:right-0' src={AuthBag.src} alt="" />
+      <img src={AuthBag.src} alt="AuthBag" />
 
     </div>
   )
--- server/src/app/home/page.tsx
+++ server/src/app/home/page.tsx
@@ -7,9 +7,9 @@ const page = () => {
     <div className='overflow-x-clip relative h-[200vh] w-screen bg-gradient-to-b from-[#FFD07E]/60 to-[#FF9A41]/60  overflow-y-hidden'>
       
       <div className='bg-black'><Nav/></div>
-      <div className='mt-10 px-2 py-16 md:p-16 md:px-9 lg:px-16 flex justify-center items-center sm:justify-normal sm:justify-items-normal'><Landing/></div>
+      <div className='LandingHolder'><Landing/></div>
 
-      <div className='w-screen flex justify-center items-center mt-5 sm:mt-15'><div className='h-[2px] bg-orange-500 w-[80vw]'></div></div>
+      <div className='LandingLine'><div></div></div>
 
     </div>
   )
