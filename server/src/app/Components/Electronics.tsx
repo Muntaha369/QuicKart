100b14f1843326b7e1b2da8e308664f6a9b418c6
Muntaha <muntahasayed396@gmail.com>
2025-08-01 23:55:54 +0530

    Done with banner

--- server/src/app/Components/Electronics.tsx
+++ server/src/app/Components/Electronics.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import ElecDealmd from '../../../public/ElecDealmd.png'
+import ElecDealsm from '../../../public/Elecdealsm.png'
+import { Julius_Sans_One } from 'next/font/google';
+import { plugin } from 'mongoose';
 // --- Reusable Components ---
 
 // 1. A reusable component for the product card skeleton
+
+const JuliusSansOne = Julius_Sans_One({subsets:['latin'], weight:['400']})
+
 const ProductCardSkeleton = () => {
   return (
     <div className='w-full aspect-[4/5] hover:cursor-pointer active:scale-100 bg-[#FFEB9E] rounded-lg  overflow-hidden border-2 hover:scale-105 transition-all'>
@@ -20,15 +26,40 @@ const ProductCardSkeleton = () => {
 // 2. A component for the top banner
 const HeroBanner = () => {
   return (
-    <div className='w-full h-[35vw] sm:h-[25vw]  bg-gray-900 rounded-2xl mb-12 shadow-lg'>
-      <div className='flex flex-col'>
-        <h1 className='text-6xl w-20 font-bold bg-gradient-to-r from-[#665700] to-[#FFD700] text-transparent bg-clip-text'>Super Sonic</h1>
-        <h1 className='inline bg-gradient-to-r text-2xl w-20 from-[#665700] to-[#FFD700] bg-clip-text text-transparent'>DEALS</h1>
-        <p>Buy Electronics Gadgets</p>
-        <p>under 30 000</p>
+    <div className='w-full flex  items-center h-[20rem] py-3.5 px-6 md:px-12 bg-black rounded-2xl mb-12 shadow-lg overflow-hidden'>
+      <div className='flex flex-col w-[50%] z-10'>
+        <h1 className=' text-5xl sm:text-6xl font-bold mt-0 bg-gradient-to-r from-[#806d01] to-[#FFD700] text-transparent w-[21rem] bg-clip-text'>Super Sonic</h1>
+        <h1 className={` ${JuliusSansOne.className} inline pt-2 bg-gradient-to-r text-4xl w-28 from-[#665700] to-[#FFD700] bg-clip-text text-transparent`}>DEALS</h1>
+        <p className='text-transparent mt-6 font-bold text-2xl bg-gradient-to-r from-[#606060] to-white w-68 bg-clip-text'>Buy Electronics Gadgets</p>
+        <p className='text-transparent font-bold text-2xl bg-gradient-to-r from-[#606060] to-white w-68 bg-clip-text'>under 30 000</p>
 
-        <button>BUY NOW</button>
+        <div className="rounded-lg mt-7 hover:scale-105 transition-all duration-300 active:scale-100 w-42 p-[2px] bg-gradient-to-r from-[#806d01] mb-4 to-[#FFD700]">
+
+          {/* 2. The Inner button has its own background */}
+          <button className="
+            w-full font-bold
+            px-6 py-2 rounded-md
+            hover:cursor-pointer
+            group">
+
+            <span className="
+              bg-gradient-to-r from-[#665700] to-[#FFD700]
+              bg-clip-text text-black/75">
+              BUY NOW
+            </span>
+            
+          </button>
+
+        </div>
       </div>
+
+      <img 
+      className='hidden md:flex'
+      src={ElecDealmd.src} alt="Elecdeal" />
+
+      <img
+      className='z-0'
+      src={ElecDealsm.src} alt="Elecdeal" />
     </div>
   );
 };
